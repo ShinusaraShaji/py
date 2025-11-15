e943274f42c0c5dc075197cee0e739ccb4191058
ShinusaraShaji <shinushaji306@gmail.com>
2025-11-15 10:24:50 +0000

    committed

---
+++ fileline.py
@@ -0,0 +1,9 @@
+filename = "one.txt"
+lines_list = []
+
+with open(filename, "r") as file:
+    for line in file:
+        lines_list.append(line.strip())   
+
+print("Lines in the list:")
+print(lines_list)
